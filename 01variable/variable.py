35395bb8aa961fc94edcbb8459d2dd877c6ae503
mahesh kumar chhedal <mheskshedal11@gmail.com>
2026-02-13 12:55:43 +0545

    Added variables and basic data types example

---
+++ 01variable/variable.py
@@ -0,0 +1,37 @@
+'''
+
+Variable= a container for a value (stirng, integer, float, boolean ) 
+          a variable behaves as if it was the value it contains
+
+'''
+#string 
+first_name='Raj '
+print(first_name)
+print(f"hello {first_name}")
+
+#integers
+age=25
+quantity=3
+print(f'you are {age} years old')
+print(f'you are buying {quantity} items')
+
+#Float  :decimal number
+price =10.99
+gpa=3
+print(f'the price is {price}')
+print(f'you gpa is {gpa}')
+
+
+#boolean: true of false 
+is_student=True
+
+for_sales=False
+
+is_online=True
+
+if is_online:
+    print('you are online ')
+else:
+    print('you are offline')
+
+
