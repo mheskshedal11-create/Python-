83ac318dab78a1bd14be0aad442515698fd3c01a
mahesh kumar chhedal <mheskshedal11@gmail.com>
2026-02-13 13:19:18 +0545

    Practice user input in Python

---
+++ 01variable/userinput.py
@@ -0,0 +1,18 @@
+'''
+userinput:
+
+'''
+name=input('What is you name: ')
+print(f'my name is {name}')
+
+age=int(input('how old are you: '))+1
+if age==False:
+    print("Please Enter valide age ")
+print(f'i am {age} old')
+
+# are of rectangle 
+length = float(input('Enter length of rectangle: '))
+breadth = float(input('Enter breadth of rectangle: '))
+
+area = length * breadth
+print(f'The area of the rectangle is {area}')
